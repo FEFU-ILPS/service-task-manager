36481b6fb7e4c52587e3ba8b53ea0c5f79b9e3c1
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-04-13 20:27:29 +1000

    feature: Конфигурация FastAPI приложения (#1)

---
+++ app.py
@@ -0,0 +1,18 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from database import disconnect_db
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # on_startup
+
+    yield
+
+    # on_shutdown
+    await disconnect_db()
+
+
+service = FastAPI(lifespan=lifespan)
