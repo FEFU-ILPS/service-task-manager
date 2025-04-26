f79792aa90a4157486a7f9c029118e55b85566e6
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-04-26 13:58:17 +1000

    feature: Конфигурация подключения сервисов аудио обработки (#4, #5)

---
+++ configs/services.py
@@ -0,0 +1,28 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class ServiceConfiguration(BaseSettings):
+    # ! Обязательные переменные
+    HOST: str
+    PORT: int
+
+    # * Опциональные переменные
+    PROTOCOL: str = "http"
+
+    @property
+    def URL(self) -> str:
+        return f"{self.PROTOCOL}://{self.HOST}:{self.PORT}"
+
+
+def get_service_configuration(service_name: str) -> ServiceConfiguration:
+    env_namespace = f"MANGER_SERVICE_{service_name.upper()}_"
+
+    class SpecificServiceConfiguration(ServiceConfiguration):
+        model_config = SettingsConfigDict(env_prefix=env_namespace)
+
+    return SpecificServiceConfiguration()
+
+
+class ServicesConfiguration(BaseSettings):
+    # * Вложенные группы настроек
+    preprocessing: ServiceConfiguration = get_service_configuration("preprocessing")
