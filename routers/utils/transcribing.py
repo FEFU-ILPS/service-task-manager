398f87676b77cc52384c3f962c424a2b045de139
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-04-27 18:43:37 +1000

    feature: Подзапрос на транскрибирование файла

--- routers/utils/transcribing.py
+++ routers/utils/transcribing.py
@@ -1,22 +1,23 @@
-import asyncio
+from io import BytesIO
 
 import httpx
 from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from configs import configs
 from database.models import Task
 from database.types import Status
 
 
-async def transcribe_audio(audio_file: bytes, task_obj: Task, db: AsyncSession) -> str:
-    """Функция передает байты аудиофайла на сервис транскрибирования,
+async def transcribe_audio(audio_file: BytesIO, task_obj: Task, db: AsyncSession) -> str:
+    """Функция передает поток байтов аудиофайла на сервис транскрибирования,
     параллельно меняя статус задачи.
 
     Функция при помощи сервиса транскрибирования возвращает строку,
     которая является фонетической записью прочитанного текста.
 
     Args:
-        audio_file (bytes): Байты аудиофайала.
+        audio_file (BytesIO): Поток байтов аудиофайала.
         task_obj (Task): Обьект ORM задачи.
         db (AsyncSession): Обьект сессии базы данных.
 
@@ -28,13 +29,25 @@ async def transcribe_audio(audio_file: bytes, task_obj: Task, db: AsyncSession) 
 
     async with httpx.AsyncClient() as client:
         try:
-            # TODO: Написать логику запроса на транскрибирование
-            await asyncio.sleep(15)  # Имитация обработки
+            response = await client.post(
+                f"{configs.services.transcribing.URL}/",
+                # TODO: Позже адаптировать возможнсть выбора других языков
+                data={"lang": "english"},
+                files={
+                    "file": (
+                        "audio.wav",
+                        audio_file.getvalue(),
+                        "audio/wav",
+                    )
+                },
+            )
+
+            response.raise_for_status()
+
+            return response.json()["transcription"]
 
         except httpx.HTTPStatusError as e:
             raise HTTPException(
                 status_code=e.response.status_code,
                 detail=e.response.json().get("detail", "Unknown error"),
             )
-
-    return "aaaaa"
